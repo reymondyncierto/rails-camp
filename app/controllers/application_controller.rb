812faf082f9c8fc1447fc79018d0a561d498b4ec
Reymond Yncierto <rpyncierto@up.edu.ph>
2024-11-07 11:00:18 +0800

    chore: ADD goodbye controller and set to root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   def hello
-    render html: "hello, world!"
+    render html: "¡Hola, mundo!"
+  end
+
+  def goodbye
+    render html: "goodbye, world!"
   end
 end
